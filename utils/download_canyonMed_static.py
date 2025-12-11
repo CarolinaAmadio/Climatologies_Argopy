f26cf6772e6e4047ea52098850914adb4ca08e44
CarolinaAmadio <caio.spoleto@gmail.com>
2025-12-11 14:40:37 +0100

    Modified script to download static directory from argopy github repo

--- utils/download_canyonMed_static.py
+++ utils/download_canyonMed_static.py
@@ -1,36 +1,42 @@
 import os
-import urllib.request
+import shutil
+import subprocess
 
-# This script downloads the required canyon-med dependency files
+# This script downloads the entire "assets" folder from argopy GitHub
 # into the Python environment where argopy is installed.
-# Use it if argopy is installed but the canyon-med weight files are missing.
 
-# path to download the canyon-med dependencies in my env
-canyon_med_dir = os.path.expanduser(
-    '~/envs/py38_seaborn/lib/python3.8/site-packages/argopy/static/assets/canyon-med/'
-)
+# Path to your Python environment
+venv_dir = os.path.expanduser('~/envs/py38_seaborn')
+assets_dir = os.path.join(venv_dir, 'lib/python3.8/site-packages/argopy/static/')
+target_assets_dir = os.path.join(assets_dir, 'assets')
 
-os.makedirs(canyon_med_dir, exist_ok=True)
+os.makedirs(assets_dir, exist_ok=True)
 
-# filelist
-files = [
-    'moy_phos_F.txt', 'std_phos_F.txt',
-    'moy_doxy_F.txt', 'std_doxy_F.txt',
-    'moy_chla_F.txt', 'std_chla_F.txt',
-    'moy_no3_F.txt', 'std_no3_F.txt',
-    'moy_no2_F.txt', 'std_no2_F.txt',
-    'moy_nh4_F.txt', 'std_nh4_F.txt'
-]
+# Temporary directory to clone the repo
+tmp_dir = '/tmp/argopy_tmp'
 
-base_url = 'https://raw.githubusercontent.com/euroargodev/argopy/master/argopy/static/assets/canyon-med/'
+# Clone the argopy repo
+if not os.path.exists(tmp_dir):
+    print("Cloning argopy repository...")
+    subprocess.run(['git', 'clone', '--depth', '1',
+                    'https://github.com/euroargodev/argopy.git', tmp_dir], check=True)
+else:
+    print("Temporary repo already exists, pulling latest changes...")
+    subprocess.run(['git', '-C', tmp_dir, 'pull'], check=True)
 
-for f in files:
-    local_path = os.path.join(canyon_med_dir, f)
-    if not os.path.exists(local_path):
-        print(f'Downloading {f}...')
-        urllib.request.urlretrieve(base_url + f, local_path)
-    else:
-        print(f'{f} already exists')
+# Copy the entire assets folder
+src_assets = os.path.join(tmp_dir, 'argopy/static/assets')
+if os.path.exists(src_assets):
+    if os.path.exists(target_assets_dir):
+        print("Removing existing assets folder...")
+        shutil.rmtree(target_assets_dir)
+    print("Copying assets folder into environment...")
+    shutil.copytree(src_assets, target_assets_dir)
+else:
+    print("assets folder not found in repository!")
 
-print("files are ready for canyon-med ")
+# Clean up temporary clone
+shutil.rmtree(tmp_dir)
+
+print("All assets are ready in your Python environment!")
 
