eef9f617c732ef62e5b15613f888fc30f3efc201
CarolinaAmadio <caio.spoleto@gmail.com>
2025-12-11 14:21:11 +0100

    Add setup script to activate venv and set PYTHONPATH for bit.sea

---
+++ utils/sequence.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+# Activate the Python virtual environment
+source /g100/home/userexternal/camadio0/envs/py38_seaborn/bin/activate
+
+# add bitsea to the PYTHONPAH
+export PYTHONPATH=/g100_scratch/userexternal/camadio0/ARGOPY_TESTS/bit.sea/src:$PYTHONPATH
+
+
